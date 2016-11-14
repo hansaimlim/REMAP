7d9afd9c753e97f8fc7630fbbe632b4bf786eb7c
Hansaim Lim <hansaimlim@gmail.com>
2016-11-14 17:33:50 -0500

    normalize and filter by cutoff score

--- matlab_codes/WeightNormalize.m
+++ matlab_codes/WeightNormalize.m
@@ -1,7 +1,9 @@
-function P=WeightNormalize(P,R)
+function P=WeightNormalize(P,R,cutoff)
 %function to normalize prediction score from REMAP
 %Normalization weight 5.25 by default
 %weight derived based on ZINC_ChEMBL dataset with negative examples
+%only include predictions with normalized_score > cutoff
+
 %P: prediction score matrix
 %R: observed pair adjacent matrix
 %bins=[0 to 1.10], bin width=0.05
@@ -13,27 +15,35 @@ for bin_idx=1:length(bin_centers)
     bin_positive=sum(R((P>(bin_centers(bin_idx)-0.025)) & P<((bin_centers(bin_idx)+0.025))));
     bin_total=length(R((P>(bin_centers(bin_idx)-0.025)) & P<((bin_centers(bin_idx)+0.025))));
     bin_negative=bin_total-bin_positive;
-    bin_weights(1,bin_idx)=(bin_positive/(bin_positive+5.25*bin_negative)); %adjusted score for the bin
+    bin_weights(1,bin_idx)=(bin_positive/(bin_positive+wt*bin_negative)); %adjusted score for the bin
 end
 
 [n,m]=size(P);
-total=n*m; %total number of pairs for progress report
+minNormScore=min(bin_weights(bin_weights > cutoff));
+minBinCenter=bin_centers(bin_weights==minNormScore);
+minRawScore=minBinCenter-0.025; %minimum raw score required for positive prediction
+nzcount=sum(P(:)>=minRawScore);
 count=0;
-tenpercent=round(total/10);
+tenpercent=round(nzcount/10);
 for i=1:n
     for j=1:m
         score=P(i,j);
-        for bin_idx=1:length(bin_centers)
-            if (score>(bin_centers(bin_idx)-0.025) && score<(bin_centers(bin_idx)+0.025) )
-                P(i,j)=bin_weights(bin_idx); %new score (adjusted)
-                count=count+1;
-                if mod(count,tenpercent)==0
-                     disp(['Normalization... ' num2str(10*count/tenpercent) ' percent done']);
+        if score < minRawScore
+            P(i,j)=0;
+        else
+            for bin_idx=1:length(bin_centers)
+                if (score>(bin_centers(bin_idx)-0.025) && score<(bin_centers(bin_idx)+0.025) )
+                    P(i,j)=bin_weights(bin_idx); %new score (adjusted)
+                    count=count+1;
+                    if mod(count,tenpercent)==0
+                         disp(['Normalization... ' num2str(10*count/tenpercent) ' percent done']);
+                    end
+                    break
                 end
-                break
             end
         end
     end
 end
-
+P=sparse(P);
+disp('Normalization complete!');
 end
