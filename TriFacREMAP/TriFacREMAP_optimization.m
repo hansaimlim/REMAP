1fa84314bda5343c141b59096ecc504f85182340
Hansaim Lim <hansaimlim@gmail.com>
2017-11-03 13:37:43 -0400

    Update TriFacREMAP_optimization.m

--- TriFacREMAP/TriFacREMAP_optimization.m
+++ TriFacREMAP/TriFacREMAP_optimization.m
@@ -1,10 +1,5 @@
 function optParams=TriFacREMAP_optimization(R, chem_chem_mat, prot_prot_mat,outfile)
-%[1] Hansaim Lim, Aleksandar Poleksic, Hanghang Tong, Yuan Yao, Di He, Luke Zhuang, Patrick Meng, and Lei Xie, "Large-scale Off-target Identification Using Fast and Accurate Dual Regularized One-Class Collaborative Filtering and Its Application to Drug Repurposing" , under review
-%chem_chem_mat and prot_prot_mat must be square matrices
-%number of rows and number of columns of R must match the number of chemicals and proteins, respectively
-%prediction matrix Y does NOT show scores for known associations
-%in other words, scores for known association pairs are set to 0
-%Please refer to the paper for detail
+%Optimize parameters by cross-validation
 %maxNumCompThreads(2); %determine the maximum number of cores to use
 kFold = 10;
 fid=fopen(outfile,'at+');
